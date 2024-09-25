b24867c84e5679b1e7c999c702dbfa1297355c1e
Louis Burress <louis.burress@gmail.com>
2024-09-24 20:54:43 -0700

    Added github keys8

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,11 +73,11 @@ resource "aws_instance" "coach_seqoiya_web" {
                 sudo yum update -y
                 sudo yum install git -y
                 sudo amazon-linux-extras install docker -y
-                sudo systemctl start docker
-                sudo systemctl enable docker
+                sudo systemctl start docker       
+         	sudo systemctl enable docker
+		sudo usermod -aG docker $USER
                 sudo curl -L "https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
                 sudo chmod +x /usr/local/bin/docker-compose
-                sudo usermod -aG docker $USER
 		sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
               EOF
 
